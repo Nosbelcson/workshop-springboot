9cb4101ff81ca9cedb2be3a762e59a067ff6d944
Clebson <clebsondavid2015@gmail.com>
2025-06-22 14:36:29 -0300

    User delete

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -26,4 +26,8 @@ public class UserService {
     public User inset(User obj) {
         return userRepository.save(obj);
     }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
 }
